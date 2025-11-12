b06e50a14b60a4270cb2228b3c6d05dc62a7b47f
igorn2659 <igorn2659@gmail.com>
2025-11-12 16:48:53 +0000

    correct files

--- api.php
+++ api.php
@@ -1,12 +1,6 @@
 <?php
-define('PATH_KEITARO_ORDERS_API_SRC','../../../orders_api/src/');
-require_once(PATH_KEITARO_ORDERS_API_SRC.'send_order.php');
-
-$thxPage = 'success.html';
-$dimensionName = 'default';
-
-if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
-    exit('Invalid request');
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    header('Location: success.html');
+    exit;
 }
-
-sendOrder($dimensionName, $thxPage, $_POST);
+?>
